ea68c4a2c334763af199d7816d15a4eefb010d5f
Nuru <ceruks@eteb.me>
2025-10-03 20:03:59 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/apolloclient/main.go
@@ -0,0 +1,23 @@
+// cmd/apolloclient/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "apolloclient/internal/apolloclient"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := apolloclient.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
